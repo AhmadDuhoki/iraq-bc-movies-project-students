e72b79b80e7e0d638b0b69a51138d4f19ff61be1
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-06-28 23:33:39 +0300

    Updates

---
+++ precache-manifest.bca6c8892d10c108014c325e002878bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5ad3e5e41d28859046d61cd276c597",
+    "url": "/iraq-bc-movies-project-students/index.html"
+  },
+  {
+    "revision": "72984377a41ad97d5543",
+    "url": "/iraq-bc-movies-project-students/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1e3d04fdc69104d13ae8",
+    "url": "/iraq-bc-movies-project-students/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "72984377a41ad97d5543",
+    "url": "/iraq-bc-movies-project-students/static/js/2.86769c8a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/iraq-bc-movies-project-students/static/js/2.86769c8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e3d04fdc69104d13ae8",
+    "url": "/iraq-bc-movies-project-students/static/js/main.5fc5960a.chunk.js"
+  },
+  {
+    "revision": "087a66ed1e9feac1f46c",
+    "url": "/iraq-bc-movies-project-students/static/js/runtime-main.83e3f6c4.js"
+  }
+]);
