5cf99b585bdfd9c6782aece9c399b47ae21297dc
Mahmoud Mansour <mahmodlte@gmail.com>
2020-06-29 11:42:15 +0300

    some edits

--- react-assissans/src/components/MovieItem.js
+++ react-assissans/src/components/MovieItem.js
@@ -4,13 +4,14 @@ import { Card, Badge } from "react-bootstrap";
 function MovieItem(props) {
   const { item } = props;
   console.log(item);
+  const overview = item.overview.substr(0, 240);
   return (
     <div>
       <Card style={{ width: "20rem", hieght: "10rem" }}>
         <Card.Img variant="top" src={item.poster_path} />
         <Card.Body>
           <Card.Title>{item.title}</Card.Title>
-          <Card.Text>{item.overview}</Card.Text>
+          <Card.Text>{overview}</Card.Text>
           <Card.Text>
             <Badge variant="success">{item.vote_average}</Badge>
           </Card.Text>
--- react-assissans/src/components/main.js
+++ react-assissans/src/components/main.js
@@ -2,7 +2,6 @@ import React from "react";
 import MoviesGrid from "./MoviesGrid";
 
 export default function Main(props) {
-  console.log(props.moviesData);
   return (
     <React.Fragment>
       <MoviesGrid movies={props.moviesData.map((data) => data)} />
